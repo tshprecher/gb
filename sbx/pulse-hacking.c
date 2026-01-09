00aeff2b03fcaebf8d8b3059583ba02f96ab12e3
Tal Shprecher <tshprecher@gmail.com>
2026-01-09 11:57:07 -0800

    implement simple write_squarewave() fn

--- sbx/pulse-hacking.c
+++ sbx/pulse-hacking.c
@@ -6,11 +6,29 @@ pa_simple *s;
 pa_sample_spec ss;
 
 
+int write_squarewave(pa_simple *s, int freq, int seconds) {
+  uint8_t data[ss.rate*seconds];
+  int error = 0;
+  int period_length = ss.rate / freq;
+
+  for (int d = 0; d < sizeof(data); d+=period_length) {
+    for (int low = 0; low < period_length>>1; low++) {
+      data[d+low] = 0;
+    }
+    for (int high = period_length>>1; high < period_length; high++) {
+      data[d+high] = 0x80;
+    }
+  }
+
+  pa_simple_write(s, data, sizeof(data), &error);
+  return error;
+}
+
 int main() {
   ss.format = PA_SAMPLE_U8;
   ss.channels = 1;
-  //  ss.rate = 44100;
-  ss.rate = 440*10;
+  ss.rate = 44100;
+  //ss.rate = 440*10;
   int error = 0;
 
   s = pa_simple_new(NULL,               // Use the default server.
@@ -30,36 +48,11 @@ int main() {
   printf("pa_frame_size: %ld\n", pa_frame_size(&ss));
   printf("pa_bytes_per_second: %ld\n", pa_bytes_per_second(&ss));
 
-  uint8_t data[440*10];
-  for (int d = 0; d < sizeof(data); d+=10) {
-    data[d] = 0;
-    data[d+1] = 0;
-    data[d+2] = 0;
-    data[d+3] = 0;
-    data[d+4] = 0;
-
-    data[d+5] = 0x10;
-    data[d+6] = 0x10;
-    data[d+7] = 0x10;
-    data[d+8] = 0x10;
-    data[d+9] = 0x10;
-  }
-
-
-
-  /*  for (int d = 0; d < sizeof(data)/sizeof(int16_t)/2; d++) {
-    data[d] = 0;
-    data[10000-d] = 1 << 10;
-    }*/
-
-  pa_simple_write(s, data, sizeof(data), &error);
+  error = write_squarewave(s, 440, 10);
   if (error) {
     printf("error code: %d\n", error);
     //printf("error str: %s\n", pa_strerror(error));
   }
-
-
-
 
   return 0;
 }
