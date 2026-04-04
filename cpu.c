b293643001d5b189701e3ce43004dbcd17ba1b23
Tal Shprecher <tshprecher@gmail.com>
2026-04-03 21:38:53 -0700

    on the path

--- cpu.c
+++ cpu.c
@@ -356,7 +356,16 @@ int cpu_exec_instruction(struct cpu *cpu , struct inst *inst) {
 	     (int8_t) inst->args[0].value.byte,
 	     (uint8_t)(-((int8_t) inst->args[0].value.byte)));
       e = (int8_t) inst->args[0].value.byte;
-      lower = lower_8(cpu->SP);
+
+
+      cpu_clear_flag(cpu, FLAG_Z);
+      cpu_clear_flag(cpu, FLAG_N);
+
+      (e & 0xFF) + (cpu->SP & 0xFF) >= 0x100 ? cpu_set_flag(cpu, FLAG_CY) : cpu_clear_flag(cpu, FLAG_CY);
+      (e & 0xF) + (cpu->SP & 0xF) >= 0x10 ? cpu_set_flag(cpu, FLAG_H) : cpu_clear_flag(cpu, FLAG_H);
+      cpu->SP += e;
+
+      /*      lower = lower_8(cpu->SP);
       upper = upper_8(cpu->SP);
       if (e >= 0) {
 	lower = alu_add(cpu, lower, e, 0);
@@ -370,16 +379,9 @@ int cpu_exec_instruction(struct cpu *cpu , struct inst *inst) {
 	flag_cy = cpu_flag(cpu, FLAG_CY);
 
 	upper = alu_sub(cpu, upper, cpu_flag(cpu, FLAG_CY));
-      }
-      cpu_clear_flag(cpu, FLAG_Z);
-      cpu_clear_flag(cpu, FLAG_N);
-      flag_h ? cpu_set_flag(cpu, FLAG_H) : cpu_clear_flag(cpu, FLAG_H);
-      flag_cy ? cpu_set_flag(cpu, FLAG_CY) : cpu_clear_flag(cpu, FLAG_CY);
-      /*      if (e < 0)
-	      printf("DEBUG: subtracted 1 from SP: 0x%02X to get 0x%02X, flag h: %d, flag cy: %d\n", cpu->SP, bytes_to_word(lower, upper), cpu_flag(cpu, FLAG_H), cpu_flag(cpu, FLAG_CY));*/
-      if (e > 0)
-	printf("DEBUG: added 1 to SP: 0x%02X to get 0x%02X, flag h: %d, flag cy: %d\n", cpu->SP, bytes_to_word(lower, upper), cpu_flag(cpu, FLAG_H), cpu_flag(cpu, FLAG_CY));
-      cpu->SP = bytes_to_word(lower, upper);
+	}*/
+
+
       break;
     case 4:
       flag_z = cpu_flag(cpu, FLAG_Z); // store before to set back at end
